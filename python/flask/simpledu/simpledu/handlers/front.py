ab6cc0f63659175c7353b77e02863ce6f9d9f6a7
5931263123 <5931263123@163.com>
2018-01-06 13:53:44 +0800

    simpledu

---
+++ python/flask/simpledu/simpledu/handlers/front.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, url_for, flash, redirect
+from simpledu.models import Course, User
+from simpledu.forms import LoginForm, RegisterForm
+from flask_login import login_user, logout_user, login_required
+
+front = Blueprint('front', __name__)
+
+@front.route('/')
+def index():
+    courses = Course.query.all()
+    return render_template('index.html', courses=courses)
+
+@front.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, form.remember_me.data)
+        return redirect(url_for('.index'))
+    return render_template('login.html', form=form)
+
+@front.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('logout success', 'success')
+    return redirect(url_for('.index'))
+
+@front.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print('start')
+        print(form.username.data)
+        form.create_user()
+        print('success')
+        flash('register success', 'success')
+        return redirect(url_for('.login'))
+    else:
+        if not form.username.data.isalnum():
+            print('error')
+            flash('username must be a num or character', 'warning')
+        return render_template('register.html', form=form)
+
